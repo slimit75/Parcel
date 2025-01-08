455af60c467f071685fb7aa74b80cc5a436d3b11
Ian <hi@slimit75.me>
2025-01-08 16:29:10 -0500

    chore: add copyright notice to source file

--- parcel.py
+++ parcel.py
@@ -1,8 +1,18 @@
 """
-Parcel
+Parcel - UNOFFICIAL file generator for the Skunkscraft Updater
+Copyright 2025 Ian Ward
 
-File generator for the Skunkscraft Updater
-2024 Ian Ward
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 import fnmatch
 import os
@@ -90,5 +100,6 @@ def main():
 
 	whitelist_file.close()
 	filesize_file.close()
+
 
 main()
